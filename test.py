a04a01a2e74204729ba288c8043e683186181cf1
Karin Munoz <karinstefa@gmail.com>
2022-04-14 13:11:59 -0500

    pruebas s3

--- test.py
+++ test.py
@@ -6,11 +6,23 @@ result_set = db.execute("SELECT * FROM Voces")
 print(result_set)
 
 
-cmd = f" ffmpeg -i  https://static-cloud-project.s3.amazonaws.com/files/voz/Rene895714.ogg  -af aresample=async=1:first_pts=0   https://static-cloud-project.s3.amazonaws.com/files/voz/Rene895714.mp3"
+cmd = f" ffmpeg -i  https://static-cloud-project.s3.amazonaws.com/files/voz/file_example_OOG_1MG.ogg  -af aresample=async=1:first_pts=0   file_example_OOG_1MG.mp3"
 #'aws s3 cp s3://static-cloud-project/files/voz/Cris237098.ogg - | ffmpeg -i - -f matroska - | aws s3 cp - s3://static-cloud-project/files/voz/Cris237098.mp3'
 
 #f" ffmpeg -i  https://static-cloud-project.s3.amazonaws.com/files/voz/Rene895714.ogg  -af aresample=async=1:first_pts=0  Rene895714.mp3"
 
-'aws s3 cp https://static-cloud-project.s3.amazonaws.com/files/voz/Rene895714.ogg - | ffmpeg -i - -f matroska - | aws s3 cp - https://static-cloud-project.s3.amazonaws.com/files/voz/Rene895714.mp3'
+#'aws s3 cp https://static-cloud-project.s3.amazonaws.com/files/voz/Rene895714.ogg - | ffmpeg -i - -f matroska - | aws s3 cp - https://static-cloud-project.s3.amazonaws.com/files/voz/Rene895714.mp3'
 
 os.system(cmd)
+
+
+#%%
+import boto3
+from s3streaming import s3_open
+
+#%%
+
+
+with s3_open('s3://bucket/key', boto_session=boto3.session.Session()) as f:
+    for next_line in f:
+        print(next_line)
